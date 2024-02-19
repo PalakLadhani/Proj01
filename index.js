6be6bf9bfe9a7589989c4a6b99e80f0031a45f56
PalakLadhani <pladhani3@gmail.com>
2024-02-20 00:38:38 +0530

    todays update

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const app = express();
 const PORT = 8000;
 //Middleware -Plugin
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  // console.log("Hello from middleware 1");
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+  // return res.json({ msg: "Hello from middleware 1" });
+  next();
+});
+
 //Routes
 app.get("/users", (req, res) => {
   const html = `
